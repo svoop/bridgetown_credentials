bb4a32b566b9c1187588846df913cfc7b55a328c
Sven Schwyn <sven.schwyn@bitcetera.com>
2022-12-29 00:55:01 +0100

    Add --environment option to commands

--- lib/bridgetown_credentials/commands/credentials.rb
+++ lib/bridgetown_credentials/commands/credentials.rb
@@ -11,12 +11,16 @@ module BridgetownCredentials
       end
 
       desc "edit", "Edit the credentials"
+      option :environment, aliases: '-e'
       def edit
+        ENV['BRIDGETOWN_ENV'] = options['environment'] if options['environment']
         BridgetownCredentials::Commands.new.edit
       end
 
       desc "show", "Dump the decrypted credentials to STDOUT"
+      option :environment, aliases: '-e'
       def show
+        ENV['BRIDGETOWN_ENV'] = options['environment'] if options['environment']
         BridgetownCredentials::Commands.new.show
       end
     end
