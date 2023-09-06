f449aaf9e111279b5c34a5811e075b4232282946
Sven Schwyn <sven.schwyn@bitcetera.com>
2023-09-06 23:25:25 +0200

    Drop minitest-sound in favor of minitest-flash

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,11 +11,9 @@ require 'pathname'
 require 'minitest/autorun'
 require Pathname(__dir__).join('..', 'lib', 'bridgetown_credentials')
 
-require 'minitest/sound'
-Minitest::Sound.success = Pathname(__dir__).join('sounds', 'success.mp3').to_s
-Minitest::Sound.failure = Pathname(__dir__).join('sounds', 'failure.mp3').to_s
-
+require 'minitest/flash'
 require 'minitest/focus'
+
 class Minitest::Spec
   class << self
     alias_method :context, :describe
